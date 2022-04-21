93ae7e68ac9e489ea373aff4ab8974e0bba37115
UralimixYT <alexilord2008@gmail.com>
2022-04-21 18:16:33 -0400

    18:16/21/04/22

--- JS/buy.js
+++ JS/buy.js
@@ -3,22 +3,29 @@ import { data } from "./data.js";
 const COMMENTS_LIST = document.querySelector(".comments-list");
 const ADD_COMMENT = document.querySelector("#add-comment");
 
-const addComment = (comment) => {
-    if (comment !== "") {
+const addComment = (comment, user = undefined) => {
+    if (comment === "") return;
+    if (user === undefined) {
         COMMENTS_LIST.innerHTML += (
             `<div class="comment">
                 ${comment}
             </div>`
         );
+    } else {
+        COMMENTS_LIST.innerHTML += (
+            `<div class="comment">
+                ${user} - ${comment}
+            </div>`
+        );  
     }
 }
 
 (function () {
     data.comments.forEach(comment => {
         addComment(comment);
-    });
+    })
 }());
 
 ADD_COMMENT.addEventListener("change", ()=> {
-    addComment(ADD_COMMENT.value);
+    addComment(ADD_COMMENT.value, data.userName);
 });
--- JS/data.js
+++ JS/data.js
@@ -2,26 +2,32 @@ import { random } from "../JavaScript-Tools/random.js";
 
 export const data = {
     loadProgress: ["", ".", "..", "..."],
-    comments: [
+    comments: [],
+    usersList: [],
+    userName: ``
+};
+
+(function () {
+    const makeUser = () => {
+        let user;
+        
+        do {
+            user = `User_${random.randrange(1, 99)}`;
+        } while (data.usersList.indexOf(user) !== -1);
+    
+        data.usersList += user;
+        
+        return user;
+    }
+
+    data.comments = [
         `${makeUser()} - C'est le meilleure calculateur d'exposant que jais vu de ma vie!`,
         `${makeUser()} - Super produit! Je recommande.`,
         `${makeUser()} - Il est facile a utiliser et tres utile`,
         `${makeUser()} - J'adore ce produit il marche tres bien`,
         `${makeUser()} - Un peux cher a 30$ mais est tres utile`,
         `${makeUser()} - C'est de la merde`
-    ],
-    usersList: [],
-    userName: `User_${makeUser()}`
-};
+    ]
 
-function makeUser ()  {
-    let user;
-    
-    do {
-        user = `User_${random.randrange(1, 10)}`;
-    } while (data.userName.indexOf(user) !== -1);
-
-    return user;
-}
-
-console.log(data.userName);
+    data.userName = makeUser();
+}());
